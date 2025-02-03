e4ee85b87d31fdb738ad260a8069140377842c0a
beyarkay <boydrkane@gmail.com>
2025-02-03 21:21:23 +0200

    only add description placeholder ifnex

--- b64-to-json.py
+++ b64-to-json.py
@@ -3,23 +3,22 @@ import base64
 import zlib
 import sys
 import json
-import os
 
 def decode_blueprint(blueprint_string):
     decoded = base64.b64decode(blueprint_string[1:])  # Skip the version byte
     json_data = zlib.decompress(decoded).decode('utf-8')
 
-    # print("mutating")
     obj = json.loads(json_data)
-    obj['blueprint']['description'] = 'No description yet.'
+    if 'description' not in obj['blueprint']:
+        obj['blueprint']['description'] = 'No description yet.'
+
     for k, v in obj['blueprint'].items():
         if type(v) is list:
             print(f'    [{k}]: saving {len(obj["blueprint"]["entities"])} entities')
         else:
             print(f'    [{k}]: saving `{v}`')
-    json_data = json.dumps(obj)
+    json_data = json.dumps(obj, indent=2)
 
-    # print("writing")
     path = 'blueprints/' + blueprint_string[:20].replace('/', '_') + '.json'
     with open(path, 'w') as f:
         f.write(json_data)
